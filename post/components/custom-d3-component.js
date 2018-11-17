88b459f3d773a7fa0bdb9f0afd1b2317fae83df5
Till <tj.krueger@live.de>
2018-11-17 17:45:56 +0100

    Add names

--- post/components/custom-d3-component.js
+++ post/components/custom-d3-component.js
@@ -7,23 +7,41 @@ const size = 600;
 class CustomD3Component extends D3Component {
 
   initialize(node, props) {
+	  debugger;
     const svg = this.svg = d3.select(node).append('svg');
     svg.attr('viewBox', `0 0 ${size} ${size}`)
       .style('width', '100%')
       .style('height', 'auto');
 
-    svg.append('circle')
-      .attr('r', 20)
-      .attr('cx', Math.random() * size)
-      .attr('cy', Math.random() * size);
+//	const circle = this.circle = svg.append("circle")
+//		circle
+//      .attr('r', props.radius)
+//      .attr('cx', Math.random() * size)
+//      .attr('cy', Math.random() * size);
+
+	const data = this.data = [1,2,3,4,5,6];
+
+	  svg.append("g")
+	  .selectAll("circle")
+	  .data(data)
+	  .enter()
+	  .append("circle")
+	  .attr("r", props.radius)
+	  .attr("cx", (d) => { return  d*30;})
+	  .attr("cy", (d) => { return  d*30;});
+
   }
 
   update(props, oldProps) {
-    this.svg.selectAll('circle')
-      .transition()
-      .duration(750)
-      .attr('cx', Math.random() * size)
-      .attr('cy', Math.random() * size);
+    this.svg.selectAll('circle').
+//	  .data(this.data)
+//	  .enter()
+	  .append('circle')
+      //.transition()
+      //.duration(750)
+      .attr('cx', (d) => return Math.random() * size;)
+      .attr('cy', (d) => return Math.random() * size;)
+	  .attr("r", oldProps.radius));
   }
 }
 
